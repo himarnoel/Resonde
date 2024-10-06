b1ef4707c4ae0dd51e75316808f00af568320d26
phantomcancode <vodumeh@gmail.com>
2024-10-06 18:01:19 +0100

    privacy policy page completed

--- src/app/components/AboutUsHero.jsx
+++ src/app/components/AboutUsHero.jsx
@@ -24,41 +24,46 @@ const AboutUsHero = () => {
                     Find Your Virtual Assistant
                 </button>
             </div>
-            <div className='flex justify-end items-center gap-[5px] xl:gap-[16.8px] py-0 my-0 w-full'>
-                <div className='w-[104.14px] xl:w-[287.28px] h-[161.99px] xl:h-[446.88px] rounded-[11.57px] xl:rounded-[19.15px] relative flex flex-col justify-end items-start'>
-                    <Image src={bg} alt='Background Image' className='absolute top-0 left-0 w-[287.28px] rounded-[19.15px] h-[446.88px] z-[3px]'></Image>
+            <div className='flex justify-end items-center gap-[5px] md:gap-[8px] lg:gap-[16.8px] py-0 my-0 w-full overflow-auto'>
+                
+                <div className='w-[150px] md:w-[200px] lg:w-[287.28px] h-[250px] md:h-[350px] lg:h-[446.88px] rounded-[11.57px] md:rounded-[15px] lg:rounded-[19.15px] relative flex flex-col justify-end items-start'>
+                    <Image src={bg} alt='Background Image' className='absolute top-0 left-0 w-full h-full rounded-[11.57px] md:rounded-[15px] lg:rounded-[19.15px]'></Image>
                     <Image src={obj} alt='Object' className='absolute right-0 z-[5px]'></Image>
-                        <div className='w-[78.68px] xl:w-[217.06px] flex flex-col justify-center items-start absolute top-[194.47px] left-[22.34px] gap-[25.54px]'>
-                            <Image src={line} alt='Icon'></Image>
-                            <p className='relative z-10 text-white leading-[28.73px] text-[18px] font-light sofiaProL'>
-                                Our mission is to provide you with Virtual assistants that make your tam more efficient and achieve great results
-                            </p>
-                        </div>
-                </div>
-                <div className='h-[446.88px] flex relative w-full xl:w-[175.56px]'>
-                    <div className='w-[63.64px] xl:w-[175.56px] h-[106.45px] xl:h-[293.66px] bottom-0 flex flex-col justify-center items-center text-center bg-[#EEEEEE] rounded-[11.57px] xl:rounded-[19.15px] absolute'>
-                        <div className='w-[44.55px] xl:w-[122.89px] h-[106.45px] xl:h-[96px] absolute top-[90.97px] left-[26.33px]'>
-                            <h1 className='text-[#333333] font-semibold sofiaProR leading-[55.86px] text-[2.813rem]'>40+</h1>
-                            <p className='w-[122.89px] text-[#333333] leading-[19.95px] text-[15.96px] font-light sofiaProL'>Unique Skills available for hire</p>
-                        </div>
+                    <div className='w-[120px] md:w-[170px] lg:w-[217.06px] flex flex-col justify-center items-start absolute top-[100px] md:top-[150px] lg:top-[194.47px] left-[10px] md:left-[15px] lg:left-[22.34px] gap-[15px] md:gap-[20px] lg:gap-[25.54px]'>
+                    <Image src={line} alt='Icon'></Image>
+                    <p className='relative z-10 text-white leading-[20px] md:leading-[25px] lg:leading-[28.73px] text-[12px] md:text-[16px] lg:text-[18px] font-light sofiaProL'>
+                        Our mission is to provide you with Virtual assistants that make your team more efficient and achieve great results
+                    </p>
                     </div>
                 </div>
-                <div className='h-[64px] xl:h-[446.88px] w-full xl:w-[319.2px] flex relative'>
-                    <div className='rounded-[11.57px] xl:rounded-[31.92px] w-[80px] xl:w-[319.2px] bottom-0 h-[236.21px] absolute'>
-                        <Image className='absolute rounded-[31.92px] z-[3px] top-0 left-0' src={man} alt='Smiley Black Man'></Image>
-                        <p className='absolute z-10 top-[146.12px] left-[20.6px] text-[#D0EA50] text-[1.75rem] leading-[35px] font-semibold sofiaProR'>Unbeliveable results....</p>
+
+                <div className='h-[250px] md:h-[350px] lg:h-[446.88px] flex relative w-[100px] md:w-[130px] lg:w-[175.56px]'>
+                    <div className='w-full h-[200px] md:h-[250px] lg:h-[293.66px] bottom-0 flex flex-col justify-center items-center text-center bg-[#EEEEEE] rounded-[11.57px] md:rounded-[15px] lg:rounded-[19.15px] absolute'>
+                    <div className='w-[40px] md:w-[70px] lg:w-[122.89px] h-[80px] md:h-[90px] lg:h-[106.45px] absolute top-[50px] md:top-[80px] lg:top-[90.97px] left-[10px] md:left-[20px] lg:left-[26.33px]'>
+                        <h1 className='text-[#333333] font-semibold sofiaProR leading-[40px] md:leading-[50px] lg:leading-[55.86px] text-[1.8rem] md:text-[2.5rem] lg:text-[2.813rem]'>40+</h1>
+                        <p className='w-[70px] md:w-[100px] lg:w-[122.89px] text-[#333333] leading-[15px] md:leading-[18px] lg:leading-[19.95px] text-[12px] md:text-[14px] lg:text-[15.96px] font-light sofiaProL'>Unique Skills available for hire</p>
+                    </div>
                     </div>
                 </div>
-                <div className='h-[446.88px] w-full xl:w-[175.56px] flex relative'>
-                    <div className='w-[63.64px] xl:w-[175.56px] h-[106.45px] xl:h-[293.66px] bottom-0 flex flex-col justify-center items-center text-center rounded-[11.57px] xl:rounded-[31.92px] bg-[#C7D8B7] absolute'>
-                        <div className='absolute z-10 top-[89.23px] leaft-[26.33px] w-[122.89px]'>
-                            <h1 className='text-[#333333] font-semibold sofiaProR leading-[55.86px] text-[2.813rem]'>90%</h1>
-                            <p className='w-[122.89px] text-[#333333] leading-[19.95px] text-[15.96px] font-light sofiaProL'>Average Client Satisfaction Rate</p>
-                        </div>
+
+                <div className='h-[250px] md:h-[350px] lg:h-[446.88px] w-[100px] md:w-[200px] lg:w-[319.2px] flex relative'>
+                    <div className='rounded-[11.57px] md:rounded-[15px] lg:rounded-[31.92px] w-full h-[150px] md:h-[200px] lg:h-[236.21px] absolute bottom-0'>
+                    <Image className='absolute h-full w-full rounded-[11.57px] md:rounded-[15px] lg:rounded-[31.92px] z-[3px] top-0 left-0' src={man} alt='Smiley Black Man'></Image>
+                    <p className='absolute z-10 top-[100px] md:top-[130px] lg:top-[146.12px] left-[10px] md:left-[15px] lg:left-[20.6px] text-[#D0EA50] text-[.75rem] md:text-[1.25rem] lg:text-[1.75rem] leading-[14px] md:leading-[25px] lg:leading-[35px] font-semibold sofiaProR'>Unbelievable results....</p>
                     </div>
                 </div>
-                <div className='w-[104.14px] xl:w-[287.28px] h-[161.99px] xl:h-[446.88px] rounded-[11.57px] xl:rounded-[31.92px] relative'>
-                    <Image src={man2} alt='Black Man' className='absolute top-0 left-0 rounded-[31.92px]'></Image>
+
+                <div className='h-[250px] md:h-[350px] lg:h-[446.88px] w-[100px] md:w-[130px] lg:w-[175.56px] flex relative'>
+                    <div className='w-full h-[200px] md:h-[250px] lg:h-[293.66px] bottom-0 flex flex-col justify-center items-center text-center rounded-[11.57px] md:rounded-[15px] lg:rounded-[31.92px] bg-[#C7D8B7] absolute'>
+                    <div className='absolute z-10 top-[50px] md:top-[75px] lg:top-[89.23px] left-[10px] md:left-[20px] lg:left-[26.33px] w-[70px] md:w-[100px] lg:w-[122.89px]'>
+                        <h1 className='text-[#333333] font-semibold sofiaProR leading-[40px] md:leading-[50px] lg:leading-[55.86px] text-[1.8rem] md:text-[2.5rem] lg:text-[2.813rem]'>90%</h1>
+                        <p className='w-[70px] md:w-[100px] lg:w-[122.89px] text-[#333333] leading-[15px] md:leading-[18px] lg:leading-[19.95px] text-[12px] md:text-[14px] lg:text-[15.96px] font-light sofiaProL'>Average Client Satisfaction Rate</p>
+                    </div>
+                    </div>
+                </div>
+
+                <div className='w-[150px] md:w-[200px] lg:w-[287.28px] h-[250px] md:h-[350px] lg:h-[446.88px] rounded-[11.57px] md:rounded-[15px] lg:rounded-[31.92px] relative'>
+                    <Image src={man2} alt='Black Man' className='absolute top-0 left-0 rounded-[11.57px] md:rounded-[15px] lg:rounded-[31.92px] w-full h-full'></Image>
                 </div>
             </div>
         </div>
--- src/app/components/WhyChooseVG.jsx
+++ src/app/components/WhyChooseVG.jsx
@@ -10,31 +10,31 @@ import contracts from '../../../public/about-us/why/Virtual Garden Resonode (3).
 const WhyChooseVG = () => {
   return (
     <section className='bg-white min-h-screen w-full'>
-        <div className='bg-[#002C15] relative z-10 min-h-screen w-full rounded-tl-[24px] lg:rounded-tl-[72px] rounded-tr-[24px] lg:rounded-tr-[72px] flex flex-col justify-center items-center py-[100px] px-[24px] lg:px-[80px] gap-[50px] md:gap-[120px]'>
+        <div className='bg-[#002C15] relative z-10 min-h-screen w-full rounded-tl-[24px] lg:rounded-tl-[72px] rounded-tr-[24px] lg:rounded-tr-[72px] flex flex-col justify-center items-center py-[100px] px-[24px] xl:px-[80px] gap-[50px] md:gap-[120px]'>
 
             <h1 className='leading-[35.2px] md:leading-[61.6px] text-white font-semibold sofiaProBld text-[32px] md:text-[3.5rem] text-center'>Why Choose Virtual Garden?"</h1>
-            <div className='max-w-[1200px] md:max-w-[1280px] mx-auto flex flex-col gap-[40px] lg:gap-[80px]'>
+            <div className='lg:max-w-[1380px] xl:max-w-[1280px] mx-auto flex flex-col justify-center items-center gap-[40px] lg:gap-[80px]'>
 
-                <div className='bg-[#1E3E2A] border border-[#275037] w-[328px] sm:w-full xl:w-[1280px] h-[440px] md:h-[600px] rounded-[16px] md:rounded-[24px] relative overflow-hidden py-[50px] md:py-[100px] px-[16px] lg:px-[32px] flex flex-col xl:flex-row gap-[16px]'>
+                <div className='bg-[#1E3E2A] border border-[#275037] w-[328px] sm:w-full 2xl:w-[1280px] h-[440px] md:h-[600px] rounded-[16px] md:rounded-[24px] relative overflow-hidden py-[50px] md:py-[100px] px-[16px] lg:px-[32px] flex flex-col xl:flex-row gap-[16px]'>
                     <div className='w-full lg:w-[385px] flex flex-col justify-center items-start gap-[16px]'>
                         <h1 className='leading-[26.4px] md:leading-[52.8px] text-white font-medium sofiaProM text-[24px] md:text-[3rem]'>Global Expertise, Local Talent</h1>
                         <p className='text-[#FAFAFA] leading-[25.6px] text-[1rem] font-light sofiaProL'>Access highly skilled Nigerian professionals with diverse experience.</p>
                         <button className='w-[173px] rounded-full h-[48px] bg-white  py-[13px] text-[#121212] font-normal sofiaProL leading-[22.4px'>Get Started</button>
                     </div>
-                    <Image width={898.91} height={546.21} className='hidden xl:flex absolute top-[60px] bottom-[380px] xl:left-[380px] object-cover' src={map} alt='Map'></Image>
+                    <Image width={898.91} height={546.21} className='w-full xl:w-[898.91] hidden xl:flex absolute top-[60px] bottom-[380px] xl:left-[380px] object-cover' src={map} alt='Map'></Image>
                     <Image width={370.14} height={224.91} className='w-full flex xl:hidden absolute left-0 top-[240.67px] bottom-[-40.71px] sm:bottom-[-20.71px] object-cover' src={resMap} alt='Map'></Image>
                 </div>
 
-                <div className='flex flex-col lg:flex-row justify-between items-center gap-[40px]'>
-                    <div className='w-[328px] sm:w-full xl:w-[620px] h-[440px] md:h-[600px] rounded-[24px] border border-[#275037] bg-[#1E3E2A] relative overflow-hidden px-[30px] py-[50px]'>
+                <div className='w-full flex flex-col xl:flex-row justify-center xl:justify-between items-center gap-[40px]'>
+                    <div className='w-[328px] sm:w-full 2xl:w-[620px] h-[440px] md:h-[600px] rounded-[24px] border border-[#275037] bg-[#1E3E2A] relative overflow-hidden px-[30px] py-[50px]'>
                         <div className='w-[483px] gap-[16px] flex flex-col justify-center items-start'>
                             <h1 className='md:leading-[52.8px] text-white font-medium sofiaProM leading-[26.4px] text-[24px] md:text-[3rem]'>Cost-Effective Solutions</h1>
                             <p className='text-[#FAFAFA] leading-[25.6px] font-light sofiaProL text-[1rem]'>Hire talent at a fraction of the cost of traditional in-country staff.</p>
                         </div>
-                        <Image width={840} height={560.08} className='hidden xl:flex absolute bottom-0 top-[231.33px] left-0 right-0 object-cover' src={coin} alt='Coin'></Image>
+                        <Image width={840} height={560.08} className='hidden xl:flex absolute bottom-[-74px] top-[280.33px] left-0 right-0 object-cover' src={coin} alt='Coin'></Image>
                         <Image width={370.14} height={224.91} className='w-full flex xl:hidden absolute left-0 top-[240.67px] bottom-[-21px] md:bottom-[-12px] object-cover' src={resCoin} alt='Coin'></Image>
                     </div>
-                    <div className='w-[328px] sm:w-full xl:w-[620px] h-[440px] md:h-[600px] rounded-[24px] border border-[#275037] bg-[#1E3E2A] relative overflow-hidden px-[30px] py-[50px]'>
+                    <div className='w-[328px] sm:w-full 2xl:w-[620px] h-[440px] md:h-[600px] rounded-[24px] border border-[#275037] bg-[#1E3E2A] relative overflow-hidden px-[30px] py-[50px]'>
                         <div className='w-[483px] gap-[16px] flex flex-col justify-center items-start'>
                             <h1 className='leading-[26.4px] text-[24px] md:leading-[52.8px] text-white font-medium sofiaProM md:text-[3rem]'>Secure & Easy-to-Use Platform:</h1>
                             <p className='text-[#FAFAFA] leading-[25.6px] font-light sofiaProL text-[1rem]'>Seamless communication, task management, and secure payments all in one place.</p>
@@ -43,7 +43,7 @@ const WhyChooseVG = () => {
                     </div>
                 </div>
                 
-                <div className='bg-[#1E3E2A] border border-[#275037] w-[328px] sm:w-full xl:w-[1280px] h-[500px] md:h-[600px] rounded-[24px] relative overflow-hidden py-[50px] lg:py-[100px] px-[32px]'>
+                <div className='bg-[#1E3E2A] border border-[#275037] w-[328px] sm:w-full 2xl:w-[1280px] h-[500px] md:h-[600px] rounded-[24px] relative overflow-hidden py-[50px] lg:py-[100px] px-[32px]'>
                     <div className='lg:w-[482px] flex flex-col justify-center items-start gap-[16px]'>
                         <h1 className='leading-[26.4px] text-[24px] md:leading-[52.8px] text-white font-medium sofiaProM md:text-[3rem]'>Flexible Contracts</h1>
                         <p className='text-[#FAFAFA] leading-[25.6px] text-[1rem] font-light sofiaProL'>Work with VAs on short to long-term projects without the burden of full-time hiring to either supplement already existing team members or complete secondary projects when your employees are focused on other tasks</p>
